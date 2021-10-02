f4a217bb2fc4dc9ddf18d7928b614c9a7ee1590c
jessclinicadev <jess@clinica.dev>
2021-10-02 19:23:52 +0800

    updated firmware

--- boot.py
+++ boot.py
@@ -10,3 +10,17 @@
 # import gc
 # gc.collect()
 # test
+
+# import network
+# import webcam
+# # ESP32 Cam
+
+# wlan = network.WLAN(network.STA_IF)
+# wlan.active(True)
+# wlan.config(dhcp_hostname='esp32cam')
+
+# # SSID and password are stored in flash:
+# # micropython code ports/esp32/modnetwork.c was changed to
+# # esp_wifi_set_storage(WIFI_STORAGE_FLASH)
+# wlan.connect()
+# webcam.run()
--- fader.py
+++ fader.py
@@ -13,15 +13,15 @@ def fade():
     # frequency must be 20MHz, 40MHz, 80Mhz, 160MHz or 240MHz
     # machine.freq(160000000)
 
-    # Set our pin 2 to PWM
-    pwm = PWM(Pin(2))
+    # Set our pin 2 to PWM - 33 esp32 cam
+    pwm = PWM(Pin(33))
 
     # Frequency = 100hz
-    pwm.freq(100)
+    # pwm.freq(100)
 
     # Pulse effect
-    # pwm.freq(1)
-    # pwm.duty(512)
+    pwm.freq(60)
+    pwm.duty(int(1023*0.5))
 
     # Print increment number
     # number = 0
@@ -30,15 +30,16 @@ def fade():
     #     print(number)
     #     time.sleep(1)
 
-    while 1:
-        # Brightness between 0 and 1023
-        for brightness in range(0, 1023, 100):
-            pwm.duty(brightness)
-            # print(brightness)
-            time.sleep(0.1)
+    # while 1:
+    #     # Brightness between 0 and 1023
+    #     for brightness in range(0, 1023, 100):
+    #         pwm.duty(brightness)
+    #         # print(brightness)
+    #         time.sleep(0.1)
 
-        # Brightness between 1023 and 0
-        for brightness in range(1023, 0, -100):
-            pwm.duty(brightness)
-            # print(brightness)
-            time.sleep(0.1)
+    #     # Brightness between 1023 and 0
+    #     for brightness in range(1023, 0, -100):
+    #         pwm.duty(brightness)
+    #         # print(brightness)
+    #         time.sleep(0.1)
+fade()
